0656ecd9140229b34e582d7ca1f1a15e2c4b48ac
MohamedMG7 <mahamedabd096@gmail.com>
2025-08-12 18:28:45 +0300

    solved 657

---
+++ 657. Robot Return to Origin/solution.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool JudgeCircle(string moves) {
+        int x = 0, y = 0;
+
+        foreach(char move in moves){
+            if(move == 'L'){
+                x--;
+            }else if(move == 'R'){
+                x++;
+            }else if(move == 'U'){
+                y++;
+            }else if(move == 'D'){
+                y--;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
